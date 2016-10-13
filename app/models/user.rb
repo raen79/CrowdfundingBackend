32ddc15711633effe0d34c0c4aaaee956f007a7b
Eran Peer <eran.peer79@gmail.com>
2016-10-13 12:20:18 +0100

    added delete to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :updates
   has_many :transactions
   has_many :comments
+
+  # Handle deleted users when deleted field is true
+  acts_as_paranoid :column => 'deleted', :column_type => 'boolean'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012222906) do
+ActiveRecord::Schema.define(version: 20161013111642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20161012222906) do
     t.string   "l_name"
     t.boolean  "approved"
     t.boolean  "deleted"
+    t.index ["deleted"], name: "index_users_on_deleted", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
