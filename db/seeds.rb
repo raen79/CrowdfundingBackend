d9ab6a602822a2ef45ce2c6d96db49c9908d2c6a
Eran Peer <eran.peer79@gmail.com>
2016-12-18 18:00:17 -0800

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ## Create User
-# Random string solution from stackoverflow => http://stackoverflow.com/questions/88311/how-best-to-generate-a-random-string-in-ruby
-# Salt is 256 bits long as per recommendation of => Practical Cryptography (Ferguson, Schneier)
-salt = rand(36**64).to_s(36)
-password = BCrypt::Password.create("eran.peer79" + salt)
-user = User.create(:email => "eran.peer79@gmail.com", :f_name => "Eran", :l_name => "Peer", :password => password, :salt => salt)
+user = User.create(:email => "eran.peer79@gmail.com", :f_name => "Eran", :l_name => "Peer", :password => "eran.peer79")
 
+## Create Vote Types
 vote_type = VoteType.create([{:name => "PROJECT"}, {:name => "UPDATE"}, {:name => "COMMENT"}])
 
+## Create Comment Types
 comment_type = CommentType.create([{:name => "PROJECT"}, {:name => "UPDATE"}])
