26684377fd4449044deae1ee23fa598e25fa4330
raen79 <eran.peer79@gmail.com>
2016-12-19 04:19:44 +0000

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope 'api' do
     scope 'authentication' do
       post '/login', :to => 'authentication#login'
-      post '/register', :to => 'authentication#register'
+      put '/register', :to => 'authentication#register'
     end
   end
 end
