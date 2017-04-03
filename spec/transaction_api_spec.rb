47e2a815ede637a08a56a239e76b88d187c51ae3
NathanMelly <nathan-melly@hotmail.co.uk>
2017-04-03 19:04:26 +0100

    adding error checks in transaction tests

--- spec/transaction_api_spec.rb
+++ spec/transaction_api_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe "Transaction", :type => :request do
     @token = generate_token
   end
 
+  #NEW TRANSACTION
   describe "PUT /api/transaction/" do
     it "creates a new transaction" do
       params = {
@@ -37,6 +38,27 @@ RSpec.describe "Transaction", :type => :request do
     end
   end
 
+  #NEW TRANSACTION FAIL
+  describe "PUT /api/transaction/" do
+    it "fails at creating a new transaction" do
+      params = {
+        :project_id => 1
+      }
+
+      put '/api/transaction/',
+        :params => params.to_json,
+        :headers => {"Token" => @token, 'Content-Type': 'application/json'}
+
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq 400
+      expect(body['error']).to eq 'amount missing'
+      expect(body['cause']).to eq 'param'
+
+    end
+  end
+
+  #REFUND TRANSACTION
   describe "PUT /api/refund_transaction/" do
     before do
       create :transaction, :id => 2, :created_at => "2017-03-12 12:40:20.206749", :amount => 470, :project_id => 1, :user_id => 1
@@ -56,6 +78,32 @@ RSpec.describe "Transaction", :type => :request do
     end
   end
 
+  #REFUND TRANSACTION FAIL
+  describe "PUT /api/refund_transaction/" do
+    before do
+      create :transaction, :id => 2, :created_at => "2017-03-12 12:40:20.206749", :amount => 470, :project_id => 1, :user_id => 1
+    end
+
+    it "refunds an existing transaction" do
+      params = {
+        :transaction_id => 2
+      }
+
+      put '/api/refund_transaction/',
+        :params => params.to_json,
+        :headers => {"Token" => @token, 'Content-Type': 'application/json'}
+
+    
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq 400
+      expect(body['error']).to eq 'amount missing'
+      expect(body['cause']).to eq 'param'
+
+    end
+  end
+
+  #SEARCH TRANSACTIONS
   describe "GET /api/transaction/" do
     before do
       create :transaction, :id => 2, :created_at => "2017-03-12 12:40:20.206749", :amount => 470, :project_id => 1, :user_id => 1
@@ -76,6 +124,34 @@ RSpec.describe "Transaction", :type => :request do
       expect(body['token'])
       expect(body['transactions']) == [{"id" => 2, "date" => "2017-03-12 12:40:20.206749", "amount" => 470, "project_id" => 1, "user_id" => 1}]
       expect(response.status).to eq 200
+
+    end
+  end
+
+  #SEARCH TRANSACTIONS FAIL
+  describe "GET /api/transaction/" do
+    before do
+      create :transaction, :id => 2, :created_at => "2017-03-12 12:40:20.206749", :amount => 470, :project_id => 1, :user_id => 1
+    end
+
+    it "returns specific transactions" do
+      params = {
+        :filter => [{"column":"amount", "operator":"isnotnull"}].to_json,
+        :page => 1,
+        :page_size => 15
+      }
+
+      get '/api/transaction/',
+        :headers => {"Token" => @token},
+        :params => params
+      
+
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq 400
+      expect(body['error']).to eq 'order missing'
+      expect(body['cause']).to eq 'param'
+
 
     end
   end
