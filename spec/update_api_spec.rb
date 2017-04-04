9d68f614d329da5b9ba4ef5b3d42e6c17072abac
Matt-Wood <WoodM2@cardiff.ac.uk>
2017-04-04 01:05:57 +0100

    Added tests for failure to update_api_spec.rb

--- spec/update_api_spec.rb
+++ spec/update_api_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "Update", :type => :request do
 			create :update, :id => 1, :created_at => "2017-03-12 12:40:20.206749", :updated_at => "2017-03-12 12:40:20.206749", :user_id => 1, :project_id => 1
 		end
 
-		it "deletes the specified users update" do
+		it "deletes the specified update" do
 			params = {
 				:id => [1]
 			}
@@ -35,6 +35,23 @@ RSpec.describe "Update", :type => :request do
 				:headers => {"Token" => @token, 'Content-Type': 'application/json'}
 			#print JSON.parse(response.body)
 			expect(response.status).to eq 200
+		end		
+	end
+
+	describe "DEL /api/update/" do 	#test delete_update for failure Matthew PASS
+		before do
+			create :update, :id => 1, :created_at => "2017-03-12 12:40:20.206749", :updated_at => "2017-03-12 12:40:20.206749", :user_id => 1, :project_id => 1
+		end
+
+		it "Failure to delete update! id is missing." do
+			params = {
+			}
+
+			delete '/api/update/',
+			:params => params.to_json,
+			:headers => {"Token" => @token, 'Content-Type': 'application/json'}
+
+			expect(response.status).to eq 400
 		end
 	end
 
@@ -65,7 +82,18 @@ RSpec.describe "Update", :type => :request do
 			expect(response.status).to eq 200
 		end
 	end
-	
+
+	describe "PUT /api/update" do #test add_update for failure Matthew PASS
+		it "failure to add update! project id is missing." do
+			params={:name => "Update test", :description => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."}
+			
+			put '/api/update',
+				:params => params.to_json,
+				:headers => {"Token" => @token, 'Content-Type': 'application/json'}
+			expect(response.status).to eq 400
+		end
+	end
+
 	describe "PATCH /api/update" do #test modify update Nathan PASS
 		before do
 			create :update, :id => 1, :created_at => "2017-03-12 12:40:20.206749", :updated_at => "2017-03-12 12:40:20.206749", :user_id => 1, :project_id => 1
@@ -82,6 +110,23 @@ RSpec.describe "Update", :type => :request do
 				:headers => {"Token" => @token, 'Content-Type': 'application/json'}
 			#print JSON.parse(response.body)
 			expect(response.status).to eq 200
+		end
+	end
+
+	describe "PATCH /api/update" do #test modify update for failure Matthew PASS
+		before do
+			create :update, :id => 1, :created_at => "2017-03-12 12:40:20.206749", :updated_at => "2017-03-12 12:40:20.206749", :user_id => 1, :project_id => 1
+		end
+
+		it "Fails to modify project! id is missing." do
+			params={
+				:name => "Modify update name",
+				:description => "MODIFIED Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."
+			}
+			patch '/api/update',
+				:params => params.to_json,
+				:headers => {"Token" => @token, 'Content-Type': 'application/json'}
+			expect(response.status).to eq 400
 		end
 	end
 end
